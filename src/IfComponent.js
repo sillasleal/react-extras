192c6cdf3a337b20aaa2a1fdc3813503dba93553
sillas <sillas@Sillas-ACN-VPC>
2019-11-12 13:09:08 -0300

    Ajustes no IfComponent

--- src/IfComponent.js
+++ src/IfComponent.js
@@ -6,42 +6,51 @@ import ComponentBase from './ComponentBase';
  * Description of IfComponent
  * @description text
  * @author Sillas S. Leal<sillas.santos.leal@accenture.com>
+ * @return {Component} Retorna o componente referente ao resultdado do teste
  * @param {Array} args
  */
-export class IfComponent extends Component {
-  render() {
-    const {children, test} = this.props;
-    /**/
-    if (isValidElement(children)) {
-      if (children.props.test && children.type.name === 'If') {
-        return children;
-      }
-    } else if (Array.isArray(children)) {
-      for (const item in children) {
-        if (children.hasOwnProperty(item)) {
-          const element = children[item];
-          if (Number(item) !== 0 && element.type.name === 'If') {
-            throw new Error('If have to be the first children in IfComponent');
-          }
-          if ((Number(item) === 0 && element.type.name === 'ElseIf') ||
-              (Number(item) === 0 && element.type.name === 'Else')) {
-            throw new Error('The first children of IfComponent' +
-                ' have to be a If');
-          }
-          if (element.props.test || element.type.name === 'Else') {
-            return element;
-          }
+export const IfComponent = ({children}) => {
+  if (isValidElement(children)) {
+    if (children.props.test && children.props.name === 'if') {
+      return children;
+    }
+  } else if (Array.isArray(children)) {
+    for (const item in children) {
+      if (children.hasOwnProperty(item)) {
+        const element = children[item];
+        if (Number(item) !== 0 && element.props.name === 'if') {
+          throw new Error('If have to be the first children in IfComponent');
+        }
+        if ((Number(item) === 0 && element.props.name === 'elseif') ||
+            (Number(item) === 0 && element.props.name === 'else')) {
+          throw new Error('The first children of IfComponent' +
+              ' have to be a If');
+        }
+        if (element.props.test || element.props.name === 'else') {
+          return element;
         }
       }
-    } else {
-      return null;
     }
+  } else {
     return null;
   }
+  return null;
 };
 
-export class If extends ComponentBase {};
+export class If extends ComponentBase {
+    static defaultProps = {
+      name: 'if',
+    }
+};
 
-export class ElseIf extends ComponentBase {};
+export class ElseIf extends ComponentBase {
+    static defaultProps = {
+      name: 'elseif',
+    }
+};
 
-export class Else extends ComponentBase {};
+export class Else extends ComponentBase {
+    static defaultProps = {
+      name: 'else',
+    }
+};
